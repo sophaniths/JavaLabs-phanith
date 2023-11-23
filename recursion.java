ac0b2318d27a46dfce0f0dd7b24fc1f82474a550
sophaniths <133214352+sophaniths@users.noreply.github.com>
2023-11-23 14:34:05 +0700

    Add files via upload

--- recursion.java
+++ recursion.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter x: ");
+        double x = input.nextDouble();
+        System.out.print("Enter n: ");
+        double n = input.nextDouble();
+        System.out.println(power(x, n));
     }
-  
+
+    public static double power(double x, double n) {
+        if (n == 0) {
+            return 1;
+        } else if (n > 0) {
+            return x * power(x, n - 1);
+        } else {
+            return 1 / (x * power(x, -n - 1));
+        }
+    }
 }
-    
